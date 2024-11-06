2ef98ef483717a5bb3db6176bb1e3fc35824e9a1
sheila wafula <167983359+cashilaa@users.noreply.github.com>
2024-11-06 19:31:15 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,14 @@ async function bootstrap() {
     key: fs.readFileSync('src/certificates/key.pem'), // Adjust path as needed
     cert: fs.readFileSync('src/certificates/cert.pem'), // Adjust path as needed
   };
-
   const app = await NestFactory.create(AppModule, { httpsOptions });
+  app.enableCors({
+    origin: 'https://elimu-global-testing.onrender.com', // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  
   await app.listen(3000); // Use HTTPS on port 5173
 }
 bootstrap();
