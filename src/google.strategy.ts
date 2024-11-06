8d27cb65b237dcf22d686bd683dbb7754caffe8c
sheila wafula <167983359+cashilaa@users.noreply.github.com>
2024-11-06 19:28:46 +0300

    Update google.strategy.ts

--- src/google.strategy.ts
+++ src/google.strategy.ts
@@ -1,30 +1,44 @@
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-google-oauth20';
-import { config } from 'dotenv';
-
-import { Injectable } from '@nestjs/common';
-
-config();
-
-@Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GoogleStrategy = void 0;
+const passport_1 = require("@nestjs/passport");
+const passport_google_oauth20_1 = require("passport-google-oauth20");
+const dotenv_1 = require("dotenv");
+const common_1 = require("@nestjs/common");
+(0, dotenv_1.config)();
+let GoogleStrategy = class GoogleStrategy extends (0, passport_1.PassportStrategy)(passport_google_oauth20_1.Strategy, 'google') {
     constructor() {
         super({
-            clientID: ,
-            clientSecret: 
-            callbackURL: ,
+            clientID: '181573529812-a0ngpmg5q0oluvo5cdh5svjrhfup8kmg.apps.googleusercontent.com',
+            clientSecret: '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',
+            callbackURL: 'https://localhost:3000/auth/google/callback',
             scope: ['email', 'profile'],
         });
     }
-    async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
-        const { name, emails, photos } = profile
+    async validate(accessToken, refreshToken, profile, done) {
+        const { name, emails, photos } = profile;
         const user = {
             email: emails[0].value,
             firstName: name.givenName,
             lastName: name.familyName,
             picture: photos[0].value,
             accessToken
-        }
+        };
         done(null, user);
     }
-}
+};
+exports.GoogleStrategy = GoogleStrategy;
+exports.GoogleStrategy = GoogleStrategy = __decorate([
+    (0, common_1.Injectable)(),
+    __metadata("design:paramtypes", [])
+], GoogleStrategy);
+//# sourceMappingURL=google.strategy.js.map
